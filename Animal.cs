f7213c8937f4b8ef74b2843f4f086cf15203df17
quang709 <69231857+quang709@users.noreply.github.com>
2020-09-14 10:44:50 +0700

    animal

---
+++ Animal.cs
@@ -0,0 +1,21 @@
+﻿namespace animal
+{
+    internal class Animal
+    {
+        private string name { get; set; }
+        private string sound { get; set; }
+
+      
+
+       public Animal (string name, string sound)
+        {
+            this.name = name;
+            this.sound = sound;
+        }
+        public string GetInfo(string name, string sound)
+        {
+
+            return $"{name} say {sound}";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace animal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sound;
+            string nameanimal;
+            
+            Console.WriteLine("nhap ten  animal:");
+             nameanimal = Console.ReadLine();
+            Console.WriteLine("nhap tieng keu  animal:");
+            sound = Console.ReadLine();
+
+            Animal animal = new Animal(nameanimal,sound);
+            Console.WriteLine(animal.GetInfo(nameanimal,sound));
+        }
+
+       
+    }
+}
